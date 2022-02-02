0e1e660978b3e3a8d595181cf59d1f9323e31bbb
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-02-02 16:03:30 +0530

    add responce status code

--- src/main/java/com/ee/employeesportal/controller/EmployeeController.java
+++ src/main/java/com/ee/employeesportal/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.ee.employeesportal.controller;
 import com.ee.employeesportal.model.Employee;
 import com.ee.employeesportal.repositories.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,13 +21,18 @@ public class EmployeeController {
     }
 
     @GetMapping("/{empId}")
-    public Employee getEmployeeById(@PathVariable(name = "empId") Long empId){
-        return employeeRepository.getEmployeeById(empId);
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable(name = "empId") Long empId){
+        Employee employeeById = employeeRepository.getEmployeeById(empId);
+        if(employeeById != null)
+            return ResponseEntity.ok(employeeById);
+
+        return ResponseEntity.badRequest().build();
     }
 
     @PostMapping("")
-    public Employee createEmployee(@RequestBody Employee employee){
-        return employeeRepository.createEmployee(employee);
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
+        employee = employeeRepository.createEmployee(employee);
+        return ResponseEntity.status(HttpStatus.CREATED).body(employee);
     }
 
     @PutMapping("/{empId}")
