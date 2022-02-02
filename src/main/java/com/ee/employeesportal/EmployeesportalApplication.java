faa33198cff0d41b56c66a925360f4b2425c4a2f
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-02-02 14:04:26 +0530

    application run file

---
+++ src/main/java/com/ee/employeesportal/EmployeesportalApplication.java
@@ -0,0 +1,13 @@
+package com.ee.employeesportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ee/employeesportal/EmployeesportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ee.employeesportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
