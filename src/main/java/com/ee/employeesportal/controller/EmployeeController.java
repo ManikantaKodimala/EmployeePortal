86dfc5ea90247a099f4ac8ba5c2af89ff6cc8a4a
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-02-06 19:25:40 +0530

    add get all employees api

--- src/main/java/com/ee/employeesportal/controller/EmployeeController.java
+++ src/main/java/com/ee/employeesportal/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.ee.employeesportal.controller;
 
 import com.ee.employeesportal.model.Employee;
 import com.ee.employeesportal.repositories.EmployeeRepository;
+import com.ee.employeesportal.services.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,37 +13,16 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/employees")
-public class EmployeeController {
-    private final EmployeeRepository employeeRepository;
 
+public class EmployeeController {
+//    private final EmployeeRepository employeeRepository;
+
+    private final EmployeeService employeeService;
     @GetMapping("")
     public List<Employee> getAllEmployees(){
-    return employeeRepository.getAllEmployees();
+//    return employeeRepository.getAllEmployees();
+       return employeeService.getAllEmployees();
     }
+    
 
-    @GetMapping("/{empId}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable(name = "empId") Long empId){
-        Employee employeeById = employeeRepository.getEmployeeById(empId);
-        if(employeeById != null)
-            return ResponseEntity.ok(employeeById);
-
-        return ResponseEntity.badRequest().build();
-    }
-
-    @PostMapping("")
-    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee){
-        employee = employeeRepository.createEmployee(employee);
-        return ResponseEntity.status(HttpStatus.CREATED).body(employee);
-    }
-
-    @PutMapping("/{empId}")
-    public Employee updateEmployeeById(@PathVariable(name = "empId") Long empId,@RequestBody Employee employee) {
-        employee.setEmpId(empId);
-        return employeeRepository.updateEmployeeById(employee);
-    }
-
-    @DeleteMapping("/{empId}")
-    public Employee deleteEmployeeById(@PathVariable(name = "empId") Long empId) {
-        return employeeRepository.deleteEmployeeById(empId);
-    }
 }
