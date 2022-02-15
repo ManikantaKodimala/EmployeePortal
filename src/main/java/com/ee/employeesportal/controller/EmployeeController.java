cf1cc771f26a81eea5642e98a2237c5c0222973e
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-02-15 18:25:26 +0530

    Refactore update url formate

--- src/main/java/com/ee/employeesportal/controller/EmployeeController.java
+++ src/main/java/com/ee/employeesportal/controller/EmployeeController.java
@@ -31,8 +31,8 @@ public class EmployeeController {
     public List<Employee> getAllEmployeesSortByDateOfJoin(){
         return employeeService.getAllEmployeesSortByDateOfJoin();
     }
-    @PutMapping("/")
-    public Employee updateEmployeeDetails(@RequestBody Employee employee){
+    @PutMapping("/{id}")
+    public Employee updateEmployeeDetails(@PathVariable("id") long id,@RequestBody Employee employee){
         return employeeService.updateEmployeeDetails(employee);
     }
 }
