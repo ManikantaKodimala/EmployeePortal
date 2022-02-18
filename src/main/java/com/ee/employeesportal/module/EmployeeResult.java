dea0aa51f4ae3394c4a4087d09fbb51251a99a8a
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-02-18 17:02:32 +0530

    add Employee result of pagenations

---
+++ src/main/java/com/ee/employeesportal/module/EmployeeResult.java
@@ -0,0 +1,29 @@
+package com.ee.employeesportal.module;
+
+
+import com.ee.employeesportal.model.Employee;
+import lombok.Data;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Data
+public class EmployeeResult {
+    private List<Employee> data;
+    private long totalElements;
+    private long totalPages;
+    private long pageSize;
+    private long currentPage;
+    private boolean hasNext;
+    private boolean hasPrevious;
+
+    public EmployeeResult(Page<Employee> employeePage){
+        this.setData(employeePage.getContent());
+        this.setTotalElements(employeePage.getTotalElements());
+        this.setTotalPages(employeePage.getTotalPages());
+        this.setCurrentPage(employeePage.getNumber());
+        this.setPageSize(employeePage.getSize());
+        this.setHasNext(employeePage.hasNext());
+        this.setHasPrevious(employeePage.hasPrevious());
+    }
+}
