414f0ee917f23e3da28e23f51a1e07c57d82652a
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2022-02-22 11:56:54 +0530

    add employee existence validation

--- src/main/java/com/ee/employeesportal/advices/ExceptionInterceptor.java
+++ src/main/java/com/ee/employeesportal/advices/ExceptionInterceptor.java
@@ -1,15 +1,36 @@
 package com.ee.employeesportal.advices;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.context.MessageSource;
 
-@RestControllerAdvice
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.*;
+
+@ControllerAdvice
 public class ExceptionInterceptor extends ResponseEntityExceptionHandler {
+    @Autowired
+    private MessageSource messageSource;
+
     @ExceptionHandler(EmployeeException.class)
-    public final ResponseEntity<Object> handleException(EmployeeException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public final ResponseEntity<Map<String ,String>> handleException(EmployeeException ex) {
+        Map<String,String> message= new HashMap<>();
+        message.put("message",ex.getMessage());
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> handleConstraintViolationException(ConstraintViolationException ex
+    ) {
+        ConstraintViolation<?> violation = ex.getConstraintViolations().iterator().next();
+        System.out.println(violation.getPropertyPath());
+        Map<String,String> message= new HashMap<>();
+        message.put("message",violation.getPropertyPath()+" is invalid");
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/ee/employeesportal/repositories/JpaEmployeeRepository.java
+++ src/main/java/com/ee/employeesportal/repositories/JpaEmployeeRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface JpaEmployeeRepository extends JpaRepository<Employee, Long> {
 
     Page<Employee> findByFirstNameContainingOrLastNameContaining(String query, String s, Pageable sortedByName);
+
+    Employee findByEverestMailId(String everestMailId);
 }
